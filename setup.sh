29f37ea7adaf6ca2cfa103cfccc83a7d6fedffef
Thales Mello <thalesmello@gmail.com>
2014-01-27 16:52:56 -0200

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Installing RVM and Ruby
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+curl -sSL https://get.rvm.io | bash -s $1
+echo source /home/vagrant/.rvm/scripts/rvm >> ~/.bashrc
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+rvm install 2.0.0
+rvm use --default 2.0.0
+gem install zeus
+rvm rvmrc warning ignore allGemfiles
+
+# Git Configurations
+git config --global color.ui true
+git config --global pull.default current
+git config --global push.default current
+git config --global user.name "Thales Mello"
+git config --global user.email thalesmello@gmail.com
+
+echo --color >> ~/.rspec
+echo --format progress >> ~/.rspec
+sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
+
+
+# Aliases
+echo alias gs=\'git status\' >> ~/.bashrc
+echo alias ga=\'git add\' >> ~/.bashrc
+echo alias gb=\'git branch\' >> ~/.bashrc
+echo alias gc=\'git commit\' >> ~/.bashrc
+echo alias gd=\'git diff\' >> ~/.bashrc
+echo alias go=\'git checkout\' >> ~/.bashrc
+echo alias gm=\'git commit -m\' >> ~/.bashrc
+echo alias be=\'bundle exec\' >> ~/.bashrc
+echo alias zc=\'zeus console\' >> ~/.bashrc
+echo alias zs=\'zeus server\' >> ~/.bashrc
+echo alias zt=\'zeus test\' >> ~/.bashrc
+echo alias zr=\'zeus rake\' >> ~/.bashrc
+echo alias mag=\'cd ~/magnetis\' >> ~/.bashrc
---
+++ setup_sudo.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+sudo echo export LANGUAGE=en_US.UTF-8 >> /etc/bash.bashrc
+sudo echo export LANG=en_US.UTF-8 >> /etc/bash.bashrc
+sudo echo export LC_ALL=en_US.UTF-8 >> /etc/bash.bashrc
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+sudo apt-get update
+sudo apt-get install -y git
+sudo apt-get install -y vim
+sudo apt-get install -y curl
+
+# PostgreSQL
+sudo apt-get install -y postgresql postgresql-client libpq-dev postgresql-contrib-9.1
+# sudo pg_dropcluster --stop 9.1 main
+# sudo pg_createcluster --start -e UTF-8 9.1 main
+sudo -u postgres createuser vagrant -s
+
+sudo apt-get install -y redis-server
+sudo apt-get install -y mongodb
+sudo apt-get install -y install memcached
+sudo apt-get install -y libxslt-dev
+sudo apt-get install -y g++
+
+# Elastic Search
+sudo apt-get install -y openjdk-7-jre-headless -y
+sudo apt-get install -y qt4-dev-tools libqt4-dev libqt4-core libqt4-gui
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.9.deb
+sudo dpkg -i elasticsearch-0.90.9.deb
+rm elasticsearch-0.90.9.deb
+
+# PhantomJS
+# sudo apt-get install -y phantomjs
+cd /usr/local/share
+sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.6-linux-x86_64.tar.bz2
+sudo tar xjf phantomjs-1.9.6-linux-x86_64.tar.bz2
+sudo ln -s /usr/local/share/phantomjs-1.9.6-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
+sudo ln -s /usr/local/share/phantomjs-1.9.6-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
+sudo ln -s /usr/local/share/phantomjs-1.9.6-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
+
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
